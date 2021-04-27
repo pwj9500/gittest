a4efc257a72d9f1cb756e2e83f5d5a9231d05b9c
pwj9500 <82165799+pwj9500@users.noreply.github.com>
2021-04-27 19:26:00 +0900

    #01 main에 출력문 추가 bs브런치 -bs-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,13 +1,9 @@
 package com.git.main;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		//var a=10;
-		int su=4;
-		Scanner sc=new Scanner(System.in);
+		System.out.println("난 BS브런치야");
 	}
 	
 }
